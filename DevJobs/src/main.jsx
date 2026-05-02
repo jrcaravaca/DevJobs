e25c617e5d551fe4512737057aa45c1184068caa
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-05-02 08:05:53 +0200

    Añade store de favoritos utilizando Zustand; implementa acciones para añadir, eliminar y gestionar favoritos.

--- DevJobs/src/main.jsx
+++ DevJobs/src/main.jsx
@@ -1,17 +1,12 @@
 
 import { BrowserRouter } from 'react-router'
-import { AuthProvider } from './context/AuthContext.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { FavoritesProvider } from './context/FavContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-      
         <App />
-
-   
   </BrowserRouter>
 )
